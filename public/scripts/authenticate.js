db8b59b29311d2e00963154984b3cb142496f431
Hugo Facchini <127160695+UInfinitu@users.noreply.github.com>
2025-11-24 22:57:29 -0300

    Atualização #14 - Hugo
    Criando acesso de Admin, diferenciando token de usuário comum

--- public/scripts/authenticate.js
+++ public/scripts/authenticate.js
@@ -1,13 +1,24 @@
 import services from '../services/index.js'
+import { auth } from '../constants'
 
 const login = async ({ email, senha }) => {
+  loginAsAdm({ email, senha })
+  if (localStorage.getItem('keyToken')) return
+
   const response = await services.api.usuarios.auth({ email, senha })
 
   if (response.status === 200) {
-    localStorage.setItem('keyToken', 'tokenLogado');
+    localStorage.setItem('keyToken', auth.USER_TOKEN);
     window.location.href = '/dashboard.html';
   } else {
     window.alert("Login Inválido");
+  }
+}
+
+const loginAsAdm = ({ email, senha }) => {
+  if (email === "adm@email.com" && senha === "adm123") {
+    localStorage.setItem('keyToken', auth.ADM_TOKEN);
+    window.location.href = '/dashboard.html';
   }
 }
 
--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,7 +1,7 @@
 import authenticate from "./authenticate.js"
 
 document.addEventListener('DOMContentLoaded', () => {
-    authenticate.validateAuth()
+  authenticate.validateAuth()
 })
 
 document.getElementById('logout').addEventListener('click', () => authenticate.logout())
